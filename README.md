b981a70e2646bda12be5e650129d1ad83fea0858
Gen <1121567132@qq.com>
2021-03-22 22:22:37 +0800

    init

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# lulu-button
+# lulu-button-test
 
 ### LICENSE
 - 使用[voices-button-cli](https://github.com/blacktunes/voices-button-cli)创建的语音按钮
-- 所用模板为[狼按钮](https://github.com/vbup-osc/mio-button)
+- 所用模板为[Hiiro按钮](https://github.com/blacktunes/hiiro-button)
--- src/components/common/Card.vue
+++ src/components/common/Card.vue
@@ -1,54 +1,39 @@
 <template>
-  <div class="card-wrapper" :class="dark ? 'dark' : 'default'">
+  <div class="card-wrapper">
     <div class="card-header" v-if="$slots.header">
       <slot name="header"></slot>
     </div>
-    <div>
+    <div class="card-text">
       <slot></slot>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  props: {
-    dark: {
-      type: Boolean,
-      default: false
-    }
-  }
-}
-</script>
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.card-wrapper
+  margin 10px 5px
+  border-radius 5px
+  border 1px solid #EBEEF5
+  background-color #FFF
+  color #303133
+  transition box-shadow 0.25s
 
-<style lang="scss" scoped>
-.card-wrapper {
-  max-width: max-content;
-  margin: 15px 30px;
-  padding: 12px 17px;
-  border-radius: 30px;
-  overflow: hidden;
-  transition: box-shadow 0.25s;
-  box-shadow: 7px 7px 14px #3a3a3a, -7px -7px 14px #626262;
-  color: #fff;
+  &:hover
+    box-shadow 2px 2px 5px rgba(0, 0, 0, 0.1)
 
-  .card-header {
-    font-size: 20px;
-    font-weight: 600;
-    margin: 2px 0;
-  }
-}
+  .card-header
+    border-bottom 1px solid #EBEEF5
+    padding 5px
 
-.default {
-  background: $card-color;
-  &:hover {
-    box-shadow: 0 10px 10px 0px rgba(0, 0, 0, 0.3);
-  }
-}
+  .card-text
+    padding 15px 10px
 
-.dark {
-  background: $dark-card-color;
-  &:hover {
-    background-image: linear-gradient(to right bottom, #464646, #535353);
-  }
-}
+@media (prefers-color-scheme dark)
+  .card-wrapper
+    background-color #444
+    color $title-color-dark
+    border 1px solid #555
+
+    .card-header
+      border-color #555
 </style>
